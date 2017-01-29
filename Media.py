aef03a804f36461f26e8c8c8987b61f084d793fe
Dominic PD <dominic.percival@gmail.com>
2017-01-29 16:56:31 +0530

    chore: remove files already put in directory The delted filed which are already in Media Website were removed

--- Media.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-#Google StyleGuide for Python
-class Movie():
-	#Constructor, always takes self as a parameter
-	def __init__(self,movie_title,movie_storyline,poster_image,trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
